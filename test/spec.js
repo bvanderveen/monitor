7be60a5084220f743750e6b7b2a39031f0e23e2b
Benjamin van der Veen <b@bvanderveen.com>
2015-06-07 16:37:28 -0700

    wrote a trivial test around cauterize

--- test/spec.js
+++ test/spec.js
@@ -1,13 +1,29 @@
 'use strict';
 
-define(['jasmine', 'app/app'], function(jasmine, app) {
+define(['jasmine', 'app/app', 'lib/libschema', 'lib/libcaut/cauterize', 'lib/libcaut/buffer', 'lib/libcaut/prototypes'], function(jasmine, app, schema, cauterize, buffer, prototypes) {
   describe('app', function(){
 
     beforeEach(function() {
     });
 
-    it('should something', function () {
-      expect(app.types).toEqual(['f32', 'quaternion', 'coordinate', 'sensor_state']);
+    it('should encode and decode messages', function () {
+        var c = new cauterize.Cauterize(schema.SpecificationInfo);
+
+        var encodedData = c.encode(new schema.PidConfigT({
+            p_gain: schema.F32.fromJS(1.0),
+            i_gain: schema.F32.fromJS(2.0),
+            d_gain: schema.F32.fromJS(3.0),
+        }));
+
+        var decodeBuffer = new buffer.CautBuffer();
+
+        decodeBuffer.append(encodedData);
+
+        var decodedObject = c.decode(decodeBuffer);
+
+        expect(decodedObject.fields.p_gain.toJS()).toEqual(1.0);
+        expect(decodedObject.fields.i_gain.toJS()).toEqual(2.0);
+        expect(decodedObject.fields.d_gain.toJS()).toEqual(3.0);
     });
   });
 });
